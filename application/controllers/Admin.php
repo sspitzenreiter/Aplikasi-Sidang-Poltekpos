15c6c8a56c83c618c043b16d3cfad2dc758b6db8
sspitzenreiter <luthfimnabil17@gmail.com>
2019-09-17 21:46:20 +0700

    Sinkron data excel ke database

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,7 +10,7 @@ class Admin extends CI_Controller {
 		if(isset($_SESSION['notification'])){
 			$con_config['notification'] = $_SESSION['notification'];
 			if(!isset($con_config['notification']['type'])){
-				$con_config['notification']['config'] = "normal";
+				$con_config['notification']['type'] = "normal";
 			}
 			$con_config['notification'] = json_encode($con_config['notification']);
 		}
@@ -43,4 +43,60 @@ class Admin extends CI_Controller {
 		$this->load->view('admin/data_dosen', $data);
 	}
 
+	public function upload_data_excel(){
+		$notification = array();
+		if(isset($_FILES['file'])){
+			$file = $_FILES['file']['tmp_name'];
+			$filename = $_FILES['file']['name'];
+			if(substr($filename, -4)==".xls" || substr($filename, -5)==".xlsx"){
+				include APPPATH.'third_party/PHPExcel/PHPExcel.php';
+				$excelreader = new PHPExcel_Reader_Excel2007();
+				$loadexcel = $excelreader->load($file); // Load file yang telah diupload ke folder excel
+				$sheet = $loadexcel->getActiveSheet()->toArray(null, true, true ,true);
+
+			// Buat sebuah variabel array untuk menampung array data yg akan kita insert ke database
+				$data = array();
+				$numrow = 0;
+				$col = array();
+				foreach($sheet as $row){
+					if($numrow>0){
+						$data_sub[$col['a']] = $row['A'];
+						$data_sub[$col['b']] = $row['B'];
+						array_push($data, $data_sub);
+					}else{
+						$col['a'] = $row['A'];
+						$col['b'] = $row['B'];
+					}
+					$numrow++;
+				}
+				$this->load->model('M_Admin');
+				$total_masuk=0;
+				foreach($data as $row){
+					$isi['npm'] = $row['npm'];
+					$isi['nama'] = $row['nama'];
+					if($this->M_Admin->insert_mahasiswa($isi)=='1'){
+						$total_masuk++;
+					}
+				}
+				if($total_masuk>0){
+					$notification['status'] = "success";
+					$notification['message'] = "Data sudah dimasukkan ke database";
+					$notification['title'] = "YEAY!";
+				}else{
+					$notification['status'] = "warning";
+					$notification['message'] = "Data tidak ada yang masuk sama sekali ke database";
+					$notification['title'] = "Aww..";
+				}
+			}else{
+				$notification['status'] = "error";
+				$notification['message'] = "File bukan termasuk excel, Coba lagi yang lain deh";
+				$notification['title'] = "Aduh";
+			}
+		}else{
+			$notification['status'] = "error";
+			$notification['message'] = "File belum terupload, Silahkan coba lagi!";
+			$notification['title'] = "Aduh";
+		}
+		echo json_encode($notification);
+	}
 }
--- application/views/admin/data_dosen.php
+++ application/views/admin/data_dosen.php
@@ -30,7 +30,7 @@
             <div class="card">
               <div class="card-header">
               	<div class="col-md-2">
-                   <button id="tolol" data-toggle="modal" data-target="#modal-default" type="button" class="btn btn-block btn-primary"><i class="fa fa-plus"></i> ADD</button>
+                   <button id="tolol" data-toggle="modal" data-target="#modal-file" type="button" class="btn btn-block btn-primary"><i class="fa fa-plus"></i> ADD</button>
                </div>
 
                 <div class="card-tools">
@@ -99,23 +99,8 @@
     <!-- /.content -->
   </div>
   <?php $this->load->view('common/footer');?>
-  <script>
-  	$(function(){
-  		$("#save").click(function(){
-  			$('#modal-default').modal('toggle');
-  			Swal.fire('Success', 'Data Submitted ', 'success');
-  		});
-  		$("#delete").click(function(){
-        alert_toast('{"title":"Coba", "text":"Mau Delete?", "status":"warning","yes_text":"Ya", "no_text":"No", "function_call":"cobain", "type":"confirmation"}');
-  			//Swal.fire('Success', 'Data Deleted', 'success');
-  		});
-  	});
-    
-    function cobain(){
-      Swal.fire('Sukses', 'Data Berhasil Dihapus', 'success');
-    }
-  </script>
-        <div class="modal fade" id="modal-default">
+
+      <div class="modal fade" id="modal-default">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
@@ -195,3 +180,77 @@
         <!-- /.modal-dialog -->
       </div>
       <!-- /.modal -->
+    </div>
+
+    <div class="modal fade" id="modal-file">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <h4 class="modal-title">Upload File</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="inputFile">File input</label>
+              <div class="input-group">
+                <div class="custom-file">
+                  <input type="file" class="custom-file-input" id="file">
+                  <label class="custom-file-label" id="file-label" for="file">Choose file</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button id="button_upload" type="button" class="btn btn-primary">Upload</button>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
+  </div>
+
+  <script>
+  	$(function(){
+  		$("#save").click(function(){
+  			$('#modal-default').modal('toggle');
+  			Swal.fire('Success', 'Data Submitted ', 'success');
+  		});
+  		$("#delete").click(function(){
+        alert_toast('{"title":"Coba", "text":"Mau Delete?", "status":"warning","yes_text":"Ya", "no_text":"No", "function_call":"cobain", "type":"confirmation"}');
+  			//Swal.fire('Success', 'Data Deleted', 'success');
+  		});
+      $('#button_upload').click(function(){
+        var filename = document.getElementById('file-label').innerHTML;
+        alert_toast('{"title":"Apakah File Sudah Benar?", "text":"Nama File : '+filename+'", "status":"warning","yes_text":"Ya", "no_text":"Tidak", "function_call":"upload_data", "type":"confirmation"}');
+      });
+      $('input[type=file]').change(function(e){
+        var filename = e.target.files[0].name;
+        document.getElementById('file-label').innerHTML=filename;
+      });
+  	});
+
+    function cobain(){
+      Swal.fire('Sukses', 'Data Berhasil Dihapus', 'success');
+    }
+
+    function upload_data(){
+      var fd = new FormData();
+      var files = $('#file')[0].files[0];
+      fd.append('file', files);
+      $.ajax({url:'<?=base_url('Admin/upload_data_excel')?>', type:'post', data: fd, contentType: false, processData: false, success: function(response){
+        alert_toast(response);
+        if(JSON.parse(response).status=="success"){
+          $('#modal-file').modal(toggle);
+        }else{
+          document.getElementById('file').value=null;
+          document.getElementById('file-label').innerHTML="Choose File..";
+        }
+      }});
+    }
+  </script>
