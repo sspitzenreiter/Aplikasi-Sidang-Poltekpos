4481c2e09ae880fbdc2e7445c582ad6c39993d74
haganz753 <aduhsakit753@gmail.com>
2019-09-18 09:53:48 +0700

    nambah di dash mhs ada format profil ama modal input a

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,12 +6,14 @@ class Mahasiswa extends CI_Controller {
 	public $con_config;
 	public function __construct(){
 		parent::__construct();
+		$this->load->model('m_mahasiswa', 'm');
 		$con_config['navigation'] = "nav_mhsw";
 		$this->con_config = $con_config;
 	}
 
 	public function index()
 	{
+		
 		$data['nav_active'] = "dashboard";
 		$data['nav_open'] = "";
 		$data = array_merge($data, $this->con_config);
@@ -33,5 +35,14 @@ class Mahasiswa extends CI_Controller {
 		$data = array_merge($data, $this->con_config);
 		$this->load->view('mahasiswa/bimb_mhsw',$data);
 		//$this->load->view('common/footer');
+	}
+	public function update(){
+		$result = $this->m->update();
+		if($result){
+			$this->session->set_flashdata('success_msg', 'Record updated successfully');
+		}else{
+			$this->session->set_flashdata('error_msg', 'Faill to update record');
+		}
+		redirect(base_url('index'));
 	}
 }
--- application/views/mahasiswa/mhsw_dash.php
+++ application/views/mahasiswa/mhsw_dash.php
@@ -4,7 +4,7 @@
 
     <section class="content-header">
       <div class="container-fluid">
-                 <div class="col-md-6">
+                 <div class="col-md-5">
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">List Kegiatan</h3>
@@ -28,14 +28,14 @@
                       <td>Proyek 3</td>
                       <td>12/02/2019</td>
 
-                      <td><button id="edit" data-toggle="modal" data-target="#modal-lg" type="button" class="btn btn-block btn-success"> IKUTI</button></td>
+                      <td><button id="edit" data-toggle="modal" data-target="#modal1" type="button" class="btn btn-block btn-success"> IKUTI</button></td>
                     </tr>
                      <tr>
                       <td>2.</td>
                       <td>SP Proyek 2</td>
                       <td>12/02/2019</td>
 
-                      <td><button id="edit" data-toggle="modal" data-target="#modal-lg" type="button" class="btn btn-block btn-success"> IKUTI</button></td>
+                      <td><button id="edit" data-toggle="modal" data-target="#modal1" type="button" class="btn btn-block btn-success"> IKUTI</button></td>
                     </tr>
                   </tbody>
                 </table>
@@ -51,6 +51,73 @@
 
     <!-- Main content -->
 
+<section class="col-lg-12 connectedSortable">
+   <div class="container-fluid">
+                 <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <div class="row">
+
+                <h5 style="font-size:30px">Profile</h5>
+              
+              <div class="col-9">
+              </div>
+              <div class="col-sm-2">
+              <button id="lengkapi" data-toggle="modal" data-target="#modal2" type="button" class="btn btn-block btn-success"> Lengkapi</button></div>
+              </div>
+            </div>
+              <div class="card-body">
+               <div class="row">
+                  <div class="col-sm-2">
+                <div class="form-group" >
+                    <label >NPM</label>
+                    <input style ="border:none" type="text" class="form-control" id="exampleInputEmail1" placeholder="NPM" readonly>
+                  </div>
+                </div>
+                <div class ="col-sm-2">
+              </div>
+                <div class="col-sm-6">
+                <div class="form-group">
+                    <label >NAMA</label>
+                    <input style ="border:none" type="text" class="form-control" id="exampleInputEmail1" placeholder="NONE" width="10px" readonly>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+              <div class="col-sm-10">
+                <div class="form-group">
+                    <label >ALAMAT</label>
+                    <input style ="border:none" type="text" class="form-control" id="exampleInputEmail1" placeholder="NONE" width="10px" readonly>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-2">
+                <div class="form-group" >
+                    <label >Angkatan</label>
+                    <input style ="border:none" type="text" class="form-control" id="exampleInputEmail1" placeholder="NONE" readonly>
+                  </div>
+                </div>
+              </div>
+                <div class="row">
+                  <div class="col-sm-3">
+                <div class="form-group" >
+                    <label >Tempat Lahir</label>
+                    <input style ="border:none" type="text" class="form-control" id="exampleInputEmail1" placeholder="NONE" readonly>
+                  </div>
+                </div>
+                 <div class="col-sm-3">
+                <div class="form-group" >
+                    <label >Tanggal Lahir</label>
+                    <input style ="border:none" type="date" class="form-control" id="exampleInputEmail1" placeholder="NONE" readonly>
+                  </div>
+                </div>
+                </div>
+            </div>
+            </div>
+          </div>
+        </div>
+</section>
 
     <!-- /.content -->
   </div>
@@ -58,16 +125,20 @@
   <script>
   	$(function(){
   		$("#save").click(function(){
-  			$('#modal-lg').modal('toggle');
+  			$('#modal1').modal('toggle');
   			Swal.fire('Success', 'Data Submitted ', 'success');
   		});
+      $("#saveprof").click(function(){
+        $('#modal2').modal('toggle');
+        Swal.fire('Success', 'Data Submitted ', 'success');
+      });
       $('#timepicker').datetimepicker({
       format: 'LT'
     });
 
   	});
   </script>
-        <div class="modal fade" id="modal-lg">
+        <div class="modal fade" id="modal1">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
@@ -95,7 +166,7 @@
                     </select>
                   </div>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Judul Proyek</label>
+                    <label >Judul Proyek</label>
                     <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Proyek">
                   </div>
                    <div class="form-group">
@@ -132,4 +203,96 @@
         </div>
         <!-- /.modal-dialog -->
       </div>
-      <!-- /.modal -->
+    </div>
+    <div class="modal fade" id="modal2">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+
+              <h4 class="modal-title">Profile</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+
+            <!-- general form elements -->
+<?php 
+      if($mahasiswa){
+        foreach($mahasiswa as $mahasiswa){
+    ?>
+
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form  action="<?php echo base_url('mahasiswa/update') ?>" method="post">
+                <div class="row">
+                  <div class="col-sm-2">
+                <div class="form-group" >
+                    <label >NPM</label>
+                    <input  type="text" class="form-control" value="<?php echo $mahasiswa->npm; ?>" name="txt_hidden" placeholder="NPM"  readonly>
+                  </div>
+                </div>
+                <div class ="col-sm-2">
+              </div>
+                <div class="col-sm-6">
+                <div class="form-group">
+                    <label >NAMA</label>
+                    <input  type="text" class="form-control" value="<?php echo $mahasiswa->nama; ?>"name="txt_namamahasiswa" placeholder="NONE" width="10px" >
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+              <div class="col-sm-10">
+                <div class="form-group">
+                    <label >ALAMAT</label>
+                    <input  type="text" class="form-control" value="<?php echo $mahasiswa->alamat; ?>"name="txt_alamatmahasiswa" placeholder="NONE" width="10px" >
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-2">
+                <div class="form-group" >
+                    <label >Angkatan</label>
+                    <input  type="text" class="form-control" value="<?php echo $mahasiswa->angkatan; ?>"name="txt_angkatanmahasiswa" placeholder="NONE" >
+                  </div>
+                </div>
+              </div>
+                <div class="row">
+                  <div class="col-sm-3">
+                <div class="form-group" >
+                    <label >Tempat Lahir</label>
+                    <input  type="text" class="form-control" value="<?php echo $mahasiswa->tempat_lahir; ?>" name="txt_tmptlhrmahasiswa" placeholder="NONE" >
+                  </div>
+                </div>
+                 <div class="col-sm-3">
+                <div class="form-group" >
+                    <label >Tanggal Lahir</label>
+                    <input  type="date" class="form-control" value="<?php echo $mahasiswa->tgl_lahir; ?>"name="txt_tgllhrmahasiswa" placeholder="NONE" >
+                  </div>
+                </div>
+                </div>
+                  
+                <!-- /.card-body -->
+
+
+              </form>
+
+
+
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button id="saveprof" type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+    </div>
+<?php
+        }
+      }
+    ?>
+
+     
